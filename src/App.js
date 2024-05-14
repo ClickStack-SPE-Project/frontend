032cabd49f7d336434867edab9b6a6cb56654fdc
Y0ungSmith <Tarun.Sharma@iiitb.ac.in>
2024-05-14 12:09:19 +0530

    restarting working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import './App.css';
 import { Login } from './components/Login';
+import { SignUp } from './components/SignUp';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <Login></Login>
+    <Router>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Login/Services/onSubmit.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+
+const BASE_URI = process.env.REACT_APP_BASE_URI
+class Users {
+    login(creds,setLoginError){ 
+        axios.post(BASE_URI + "/auth/authenticate", creds)
+        .then((response) => {
+            if (response.status === 200) {
+            localStorage.setItem("accesstoken", response.data.token);
+            setLoginError("")
+        }
+        })
+        .catch((error) => {
+            if (error.response && (error.response.status === 403 || error.response.status === 401)) {
+                setLoginError("Please enter correct credentials")
+            } else {
+                setLoginError("An unexpected error occurred. Please try again later.")
+            }
+        })
+    
+    
+  
+    }
+}
+
+const userInstance = new Users();
+
+export default userInstance;
